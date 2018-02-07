98fc98f7ba354667a1ae313fc67f4e314825d27f
AbdulSamii Ajala <jalasem@protonmail.com>
2018-02-07 08:15:52 +0100

    css adjustments

--- src/pages/platform/category.vue
+++ src/pages/platform/category.vue
@@ -1,11 +1,13 @@
 <template>
   <page :pageClasses="['categories__view', 'row']">
     <cat-nav />
-    <div class="col s12 center">
+    <div class="col s12 m8 l9 right center-align row">
       <h1>{{ categoryDetails.name }}</h1>
       <p class="flow-text">{{ categoryDetails.description }}</p>
+      <div class="col s12"></div>
+      <div class="col s12 m6 l4"></div>
     </div>
-    <div class="col s12 center center-align">
+    <div class="col s12 m4 l3 center center-align subcats">
       <div v-for="(subcategory, index) in categoryDetails.subcategories" :key="index" class="card-panel subcatCard waves-effect">
         <p class="">{{ capitalize(subcategory) }}</p>
       </div>
@@ -42,18 +44,24 @@ export default {
 h1 {
   font-size: 3rem;
 }
-p {
-  font-size: 1.3em;
-}
-.subcatCard.card-panel {
-  width: auto;
-  display: inline-block;
-  margin-left: 1em;
-  padding: 0px 24px;
-  cursor: pointer;
-  transition: background ease-in-out .3s;
-  &:hover {
-    background: rgb(240, 248, 255);
+.subcats {
+  padding-top: 2em;
+  p {
+    font-size: 1.3em;
+    line-height: 125%;
+    text-align: left;
+    margin: 0.5em 0;
+  }
+  .subcatCard.card-panel {
+    width: 100%;
+    display: inline-block;
+    margin: 1px;
+    padding: 0px 24px;
+    cursor: pointer;
+    transition: background ease-in-out .3s;
+    &:hover {
+      background: rgb(240, 248, 255);
+    }
   }
 }
 </style>
