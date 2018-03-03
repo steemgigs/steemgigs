82abef9a71a17a613b03bbc333b19c002ca8a534
AbdulSamii Ajala <jalasem@protonmail.com>
2018-03-03 15:50:22 +0100

    set prod vars

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
 
-// export default axios.create({baseURL: `https://178.63.209.34:8443`})
-export default axios.create({baseURL: `http://localhost:5000`})
-// export default axios.create({baseURL: `https://server.steemgigs.org`})
+// export default axios.create({baseURL: `http://localhost:5000`})
+export default axios.create({baseURL: `https://server.steemgigs.org`})
