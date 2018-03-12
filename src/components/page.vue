367ca4c9b535ea92da0d2d16355e26d0a74c62f4
Afeez Awoyemi <damafeez@gmail.com>
2018-03-12 20:58:22 +0100

    working

--- src/components/page.vue
+++ src/components/page.vue
@@ -28,7 +28,6 @@ export default {
     let {accessToken, username} = this.$store.state
     Api.loggedIn(username, accessToken).then(response => {
       let responseData = response.data.profile
-      console.log('FROM LOGGED IN', {responseData})
       let {social, name, profilePic, about, location, coverPic, rep, balance, steemgigsWitness} = responseData
       this.$store.commit('SET_PROFILE', {social, name, profilePic, about, location, coverPic, rep, balance, steemgigsWitness})
     })
