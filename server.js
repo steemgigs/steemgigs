b07021659f254794d6275f353d16fa5baf5e669f
Abdulsamii <abdulsamii@fluidangle.com>
2018-12-23 12:31:27 +0100

    bug fix

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use('/static', express.static(path.join(__dirname, 'dist')))
-// app.get('*', (req, res) => {
-//   res.send('./dist/index.html')
-// })
+
+app.get('/*', (req, res) => {
+  res.send('./dist/index.html')
+})
 
 const port = process.env.PORT || 5000
 app.listen(port)
