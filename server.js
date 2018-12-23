57b2fdd3f8b25419396a368b7d0bacb933f63ffd
Abdulsamii <abdulsamii@fluidangle.com>
2018-12-23 10:54:17 +0100

    bug fix

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
-const serveStatic = require('serve-static')
 app = express()
-app.use(serveStatic(__dirname + '/dist'))
+
+app.use(express.static(path.join(__dirname, 'dist')))
+// app.get('*', (req, res) => {
+//   res.send('./dist/index.html')
+// })
+
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started '+ port)
