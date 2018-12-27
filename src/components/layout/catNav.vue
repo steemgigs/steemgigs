6b6a33bfa953f63bc3fa21ff335f6ed3b39d0d70
Tobias <tobias.gooney@icloud.com>
2018-12-27 21:28:02 +0000

    Round category nav container, resolve issue with z-index with cat nav

--- src/components/layout/catNav.vue
+++ src/components/layout/catNav.vue
@@ -2,7 +2,7 @@
   <ul class="subnav center center-align hide-on-med-and-down">
     <li class="main-li" v-for="(category, index) in categories" :key="index">
       <router-link :to="'/categories/' + slugify(category.name)">{{ capitalize(category.name) }}</router-link>
-      <div class="dropdown white z-depth-1 drop">
+      <div class="dropdown white drop">
         <ul>
           <li v-for="(subcategory, subIndex) in half1(category.subcategories)" :key="subIndex">
             <router-link :to="'/categories/' + slugify(category.name) + '/' + slugify(subcategory.name)">{{ capitalize(subcategory.name) }}</router-link>
@@ -85,7 +85,6 @@ export default {
   li.main-li {
     display: inline-block;
     position: relative;
-    z-index: 100000;
     a {
       padding: 11.5px 0.5em;
       line-height: 40px;
@@ -101,12 +100,13 @@ export default {
         content: ' ';
         height: 2px;
         width: 0%;
-        background: black;
+        background: #3f50c6;
         display: inline-block;
         position: absolute;
         left: 0;
         bottom: 0;
         transition: all ease-in-out .3s;
+        box-shadow: 0 3px 13px rgba(0, 0, 0, 0.05);
       }
     }
     .dropdown {
@@ -116,25 +116,20 @@ export default {
       width: auto;
       opacity: 1;
       visibility: visible;
-      // display: block;
-      // z-index: 1;
-      // max-width: 30em;
-      // width: fit-content;
-      // text-align: left;
-      // position: fixed;
+      border-radius: 10px;
+      margin-top: 5px;
       background: #fff;
       display: -webkit-box;
       display: -ms-flexbox;
       display: flex;
       position: absolute;
-      z-index: 1;
+      z-index: 20;
       top: 100%;
       left: 50%;
       right: auto;
       vertical-align: baseline;
       border: 1px #e5e5e5 solid;
       transform: translateX(-50%);
-      // box-shadow: 0 1px 3px #dddddd;
       padding: 0 0;
       -webkit-transition: opacity .1s;
       transition: opacity .1s;
