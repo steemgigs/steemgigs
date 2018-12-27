382f1e3bf9640fc74da9b1624100002ef30a8799
Tobias <tobias.gooney@icloud.com>
2018-12-27 02:36:17 +0000

    Beautify nav.vue

--- src/components/layout/nav.vue
+++ src/components/layout/nav.vue
@@ -1,6 +1,6 @@
- <template>
+<template>
   <div class="navbar-fixed">
-    <nav class="white"  :class="searchActive ? 'search-active': ''">
+    <nav class="white" :class="searchActive ? 'search-active': ''">
       <div class="nav-wrapper container">
         <router-link to="/" class="brand-logo left"><img src="/static/img/logo.gif" alt="logo"></router-link>
         <ul class="right notIn" v-if="!$store.state.accessToken">
@@ -17,36 +17,52 @@
               <router-link to="/dashboard"><i class="icon ion-speedometer x2"></i></router-link>
             </li>
             <li>
-              <router-link to="/cart" ><i class="icon ion-bag x2"></i></router-link>
+              <router-link to="/cart"><i class="icon ion-bag x2"></i></router-link>
             </li>
             <li>
               <el-dropdown>
                 <span class="el-dropdown-link">
-                    <el-button type="primary" class="primary" size="medium">Create</el-button>
-                </span>
+                      <el-button type="primary" class="primary" size="medium">Create</el-button>
+                  </span>
                 <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item><router-link to="/create_gig">Gig</router-link></el-dropdown-item>
-                <el-dropdown-item><router-link to="/steemgigs_request">Custom Request</router-link></el-dropdown-item>
-                <el-dropdown-item><router-link to="/create_testimonial">Testimonial</router-link></el-dropdown-item>
-                <el-dropdown-item><router-link to="/untalented_editor">Untalented</router-link></el-dropdown-item>
+                  <el-dropdown-item>
+                    <router-link to="/create_gig">Gig</router-link>
+                  </el-dropdown-item>
+                  <el-dropdown-item>
+                    <router-link to="/steemgigs_request">Custom Request</router-link>
+                  </el-dropdown-item>
+                  <el-dropdown-item>
+                    <router-link to="/create_testimonial">Testimonial</router-link>
+                  </el-dropdown-item>
+                  <el-dropdown-item>
+                    <router-link to="/untalented_editor">Untalented</router-link>
+                  </el-dropdown-item>
                 </el-dropdown-menu>
-                </el-dropdown>
+              </el-dropdown>
             </li>
           </div>
           <li>
-           <el-dropdown> 
+            <el-dropdown>
               <span class="el-dropdown-link">
-             <a><img class="profile_pic" :src="profileImg" alt=""></a>
-               </span>
-               <el-dropdown-menu slot="dropdown">
-             <el-dropdown-item><router-link class="waves-effect" :to="'/@' + $store.state.username"> {{ $store.state.username + ' (' + repp + ') ' }} </router-link></el-dropdown-item>
-            <el-dropdown-item><router-link class="waves-effect" to="/wallet">Wallet - {{ wallet }}</router-link></el-dropdown-item>
-           <el-dropdown-item><router-link class="waves-effect" to="/settings">Settings</router-link></el-dropdown-item>
-             <el-dropdown-item><router-link class="waves-effect red-text" to="/invite">Invite friends</router-link></el-dropdown-item>
-             <el-dropdown-item><a class="waves-effect" href="https://discord.gg/CGuPyyT" target="_blank">Help</a></el-dropdown-item>
-              <el-dropdown-item><a @click.prevent="logout()">Log Out</a></el-dropdown-item>
-               </el-dropdown-menu>
-           </el-dropdown>
+               <a><img class="profile_pic" :src="profileImg" alt=""></a>
+                 </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>
+                  <router-link class="waves-effect" :to="'/@' + $store.state.username"> {{ $store.state.username + ' (' + repp + ') ' }} </router-link>
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <router-link class="waves-effect" to="/wallet">Wallet - {{ wallet }}</router-link>
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <router-link class="waves-effect" to="/settings">Settings</router-link>
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <router-link class="waves-effect red-text" to="/invite">Invite friends</router-link>
+                </el-dropdown-item>
+                <el-dropdown-item><a class="waves-effect" href="https://discord.gg/CGuPyyT" target="_blank">Help</a></el-dropdown-item>
+                <el-dropdown-item><a @click.prevent="logout()">Log Out</a></el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
           </li>
           <li class="hide-on-large-only">
             <a href="#" data-target="mobile-demo" class="sidenav-trigger mx-0">
@@ -57,7 +73,7 @@
       </div>
     </nav>
     <search-box />
-     <div id="loginPrompt" class="modal">
+    <div id="loginPrompt" class="modal">
       <div class="modal-content grey-text text-darken-1 login-modal">
         <h4>Redirection to SteemConnect V2</h4>
         <hr class="grey lighten-2">
@@ -115,7 +131,9 @@ export default {
       this.profile = this.$store.state.profile
     })
     let elem = document.querySelector('.modal')
-    M.Modal.init(elem, {dismissible: true})
+    M.Modal.init(elem, {
+      dismissible: true
+    })
   },
   beforeDestroy () {
     this.$eventBus.$off('profile-fetched')
@@ -163,76 +181,78 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$blue:#6361D0;
-
-nav a.brand-logo, nav li a, nav a {
-  color: $blue;
-}
-.login-modal {
-  p {
-    font-size: 1.2em
+  $blue:#6361D0;
+  nav a.brand-logo,
+  nav li a,
+  nav a {
+    color: $blue;
   }
-}
 
-nav {
-  box-shadow: 0 0;
-  border-bottom: 0px solid #e9e7e7;
-  .search-icon {
-    position: absolute;
-    right: 0;
-  }
-  .search-panel {
-    // width: 0;
-    // overflow: hidden;
-    // transition: all .7s ease-in;
-    // border-radius: 50px;
-    // border: none;
-    // background: rgb(238, 238, 238);
-    // display: inline-block;
-    // transform: translateY(-2.3px);
-    &:focus {
-      outline: 0 solid;
-      border: 1px solid rgb(203, 135, 252);
+  .login-modal {
+    p {
+      font-size: 1.2em
     }
   }
-  &.search-active {
+
+  nav {
+    box-shadow: 0 0;
+    border-bottom: 0px solid #e9e7e7;
+    .search-icon {
+      position: absolute;
+      right: 0;
+    }
     .search-panel {
-      width: 300px;
-      padding: 7px 15px;
+      // width: 0;
+      // overflow: hidden;
+      // transition: all .7s ease-in;
+      // border-radius: 50px;
+      // border: none;
+      // background: rgb(238, 238, 238);
+      // display: inline-block;
+      // transform: translateY(-2.3px);
+      &:focus {
+        outline: 0 solid;
+        border: 1px solid rgb(203, 135, 252);
+      }
     }
-    .search-dropdown {
-      display: block !important;
-      margin-top: 58px;
-      margin-left: 50px;
-      color: #333;
-      background: #eeeeef;
-      text-align: center;
-      padding: 2px 25px;
-      border-radius: 3px;
-      box-shadow: 0 3px 9px rgba(223, 223, 223, 0.2);
-      li {
-        float: none;
-        cursor: pointer;
-        transition: color .3s ease;
-        &:hover {
-          color: #6361D0;
+    &.search-active {
+      .search-panel {
+        width: 300px;
+        padding: 7px 15px;
+      }
+      .search-dropdown {
+        display: block !important;
+        margin-top: 58px;
+        margin-left: 50px;
+        color: #333;
+        background: #eeeeef;
+        text-align: center;
+        padding: 2px 25px;
+        border-radius: 3px;
+        box-shadow: 0 3px 9px rgba(223, 223, 223, 0.2);
+        li {
+          float: none;
+          cursor: pointer;
+          transition: color .3s ease;
+          &:hover {
+            color: #6361D0;
+          }
         }
       }
+      // .shrink {
+      //   width: 0;
+      // }
     }
-    // .shrink {
-    //   width: 0;
-    // }
-  }
-  .nav-wrapper {
-    &.container {
-      min-width: calc(100% - 60px);
-      a.brand-logo img {
-        height: 1em;
-        margin-top: 13px;
-      }
-      ul.notIn {
-        li a {
-          &:hover::after {
+    .nav-wrapper {
+      &.container {
+        min-width: calc(100% - 60px);
+        a.brand-logo img {
+          height: 1em;
+          margin-top: 13px;
+        }
+        ul.notIn {
+          li a {
+            &:hover::after {
               width: 100%;
             }
             &::after {
@@ -246,73 +266,76 @@ nav {
               bottom: 0;
               transition: all ease-in-out .3s;
             }
+          }
         }
-      }
-      ul {
-        li {
-          position: relative;
-          a {
-            font-weight: bold;
-            line-height: 57px;
-            transition: all ease-in-out .3s;
+        ul {
+          li {
             position: relative;
-            &.btn {
-              line-height: 35px;
-              font-size: 1.2em;
-              padding: 0 1em;
-              i.icon {
-                line-height: 38px;
-                margin-right: 0.5em;
+            a {
+              font-weight: bold;
+              line-height: 57px;
+              transition: all ease-in-out .3s;
+              position: relative;
+              &.btn {
+                line-height: 35px;
+                font-size: 1.2em;
+                padding: 0 1em;
+                i.icon {
+                  line-height: 38px;
+                  margin-right: 0.5em;
+                }
+              }
+              img.profile_pic {
+                border-radius: 50%;
+                border: 1px solid #6361D0;
+                width: 2.7em;
+                height: 2.7em;
+                margin-top: 7px;
+                // background: grey;
+                margin-left: -0.5em;
+                display: inline-block;
               }
             }
-            img.profile_pic {
-              border-radius: 50%;
-              border: 1px solid #6361D0;
-              width: 2.7em;
-              height: 2.7em;
-              margin-top: 7px;
-              // background: grey;
-              margin-left: -0.5em;
-              display: inline-block;
+            ul {
+              display: none;
             }
-          }
-          ul {
-            display: none;
-          }
-          &:hover ul {
-            padding-top: 0.3em;
-            padding-bottom: 0.3em;
-            display: block;
-            position: absolute;
-            top: 55px;
-            width: 160px;
-            right: -4em;
-            transform: translateX(-7px);
-            li {
-              a {
-                padding: 0 1em;
-                line-height: 25px;
-                display: block;
-                min-width: 8em;
-                font-size: 0.9em;
+            &:hover ul {
+              padding-top: 0.3em;
+              padding-bottom: 0.3em;
+              display: block;
+              position: absolute;
+              top: 55px;
+              width: 160px;
+              right: -4em;
+              transform: translateX(-7px);
+              li {
+                a {
+                  padding: 0 1em;
+                  line-height: 25px;
+                  display: block;
+                  min-width: 8em;
+                  font-size: 0.9em;
+                }
               }
             }
           }
         }
-      }
-
-      a.brand-logo, li a, a {
-        color: $blue;
-        &:hover {
-          background: transparent;
+        a.brand-logo,
+        li a,
+        a {
+          color: $blue;
+          &:hover {
+            background: transparent;
+          }
         }
       }
     }
   }
-}
 
-@media only screen and (min-width: 601px) {
-  nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i {
+  @media only screen and (min-width: 601px) {
+    nav,
+    nav .nav-wrapper i,
+    nav a.sidenav-trigger,
+    nav a.sidenav-trigger i {}
   }
-}
 </style>
