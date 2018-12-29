3956a4ebcad9c0b5f3bfda189a2f2e2e79e2bf91
Tobias <tobias.gooney@icloud.com>
2018-12-29 13:27:08 +0000

    Temporary fixes for layout issues with nav bar fixed

--- src/components/layout/catNav.vue
+++ src/components/layout/catNav.vue
@@ -82,6 +82,7 @@ export default {
   margin-top: 0;
   margin-bottom: 0;
   border-top: 1px solid whitesmoke;
+  position: fixed;
   li.main-li {
     display: inline-block;
     position: relative;
