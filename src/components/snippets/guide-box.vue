73ff953b1032bc57ff7b41aa291a269f42d35ec8
Tobias <tobias.gooney@icloud.com>
2019-01-05 23:25:28 +0000

    Create basic compenent for editor guide

---
+++ src/components/snippets/guide-box.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="guide-container">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'guide',
+  props: {
+  }
+}
+</script>
+<style>
+.placeholder {
+    text-align: center;
+}
+</style>
