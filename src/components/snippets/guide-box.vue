f34af104dcaf9fd2615ac9e1a178deb827beb78a
Tobias <tobias.gooney@icloud.com>
2019-01-05 23:53:02 +0000

    Add basic styling to guide component, add props and itterate through

--- src/components/snippets/guide-box.vue
+++ src/components/snippets/guide-box.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="guide-container">
+    <h3> {{ header }} </h3>
+    <h5> {{ subHeader }} </h5>
+    <iframe class="help-video" width="auto" src="https://www.youtube.com/embed/xeLYLA6C2AE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <ul class="guide-items">
+        <span class="guide-item" v-for="(item, index) in itemList" :key="index"> {{ item }}</span>
+    </ul>
 </div>
 </template>
 
@@ -7,11 +13,29 @@
 export default {
   name: 'guide',
   props: {
+    header: String,
+    subHeader: String,
+    itemList: Array
   }
 }
 </script>
 <style>
-.placeholder {
-    text-align: center;
+.guide-container {
+    background: white;
+    padding: 20px;
+    box-shadow: 0 3px 13px rgba(0, 0, 0, 0.05);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
+
+.guide-item {
+    margin: 15px 30px;
+    display: block;
+    font-size: 13px;
+    display: list-item;
+}
+
 </style>
