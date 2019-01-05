c90317be37d995153081203f7cc88ee9f5169cff
Tobias <tobias.gooney@icloud.com>
2019-01-05 15:11:09 +0000

    Create mixin to handle form actions

---
+++ src/mixins/form.js
@@ -0,0 +1,34 @@
+var form = {
+
+  methods: {
+
+  // Form Reset
+
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+
+    // Tag Methods
+
+    handleClose (userTag) {
+      this.userTags.splice(this.userTags.indexOf(userTag), 1)
+    },
+
+    showInput () {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    handleInputConfirm () {
+      let inputValue = this.inputValue
+      if (inputValue) {
+        this.userTags.push(inputValue)
+      }
+      this.inputVisible = false
+      this.inputValue = ''
+    }
+  }
+}
+
+export default form
