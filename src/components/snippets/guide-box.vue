1b4c329c00e279ce405cf52e2bf6207bf31c30f4
Tobias <tobias.gooney@icloud.com>
2019-01-06 00:41:45 +0000

    Add video id to component prop, add youtube video component to guide component

--- src/components/snippets/guide-box.vue
+++ src/components/snippets/guide-box.vue
@@ -2,40 +2,44 @@
 <div class="guide-container">
     <h3> {{ header }} </h3>
     <h5> {{ subHeader }} </h5>
-    <iframe class="help-video" width="auto" src="https://www.youtube.com/embed/xeLYLA6C2AE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <youtube :video-id="videoId" player-width="auto" player-height="auto"></youtube>
     <ul class="guide-items">
-        <span class="guide-item" v-for="(item, index) in itemList" :key="index"> {{ item }}</span>
+        <span class="guide-item" v-for="(item, index) in itemList" :key="index"> - {{ item }}</span>
     </ul>
 </div>
 </template>
 
 <script>
+
 export default {
   name: 'guide',
   props: {
     header: String,
     subHeader: String,
-    itemList: Array
+    itemList: Array,
+    videoId: String
   }
 }
 </script>
 <style>
 .guide-container {
     background: white;
-    padding: 20px;
+    padding: 30px;
     box-shadow: 0 3px 13px rgba(0, 0, 0, 0.05);
     border-radius: 10px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
 }
 
 .guide-item {
-    margin: 15px 30px;
     display: block;
-    font-size: 13px;
-    display: list-item;
+    margin-bottom: 15px;
+    font-size: 14px;
+}
+
+.guide-item:last-child {
+    margin-bottom: 0;
 }
 
 </style>
