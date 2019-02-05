266a8db2ab7eaa660bd03f651ae475997f60cf81
Tobias <tobias.gooney@icloud.com>
2019-02-05 22:48:54 +0000

    Update reblog method to take into account user status

--- src/components/snippets/gigAction.vue
+++ src/components/snippets/gigAction.vue
@@ -127,30 +127,32 @@ export default {
       }
     },
     reblog () {
-      this.rsspinning = true
-      this.resteeming = false
-      this.resteem = false
-      sc2.setAccessToken(this.$store.state.accessToken)
-      sc2.reblog(this.$store.state.username, this.gigData.author, this.gigData.permlink, (err, res) => {
-        if (!err) {
-          this.resteem = true
-          this.resteeming = true
-          this.rsspinning = false
-          this.$notify({
-            title: 'Success',
-            message: 'Post Resteem Successfully',
-            type: 'success'
-          })
-        } else {
-          this.resteeming = true
-          this.resteem = false
-          this.rsspinning = false
-          this.$notify.error({
-            title: 'Error',
-            message: 'There was an error resteeming that post.'
-          })
-        }
-      })
+      if (this.userLoggedIn()) {
+        this.rsspinning = true
+        this.resteeming = false
+        this.resteem = false
+        sc2.setAccessToken(this.$store.state.accessToken)
+        sc2.reblog(this.$store.state.username, this.gigData.author, this.gigData.permlink, (err, res) => {
+          if (!err) {
+            this.resteem = true
+            this.resteeming = true
+            this.rsspinning = false
+            this.$notify({
+              title: 'Success',
+              message: 'Post Resteem Successfully',
+              type: 'success'
+            })
+          } else {
+            this.resteeming = true
+            this.resteem = false
+            this.rsspinning = false
+            this.$notify.error({
+              title: 'Error',
+              message: 'There was an error resteeming that post.'
+            })
+          }
+        })
+      }
     },
     upvote () {
       this.voting = true
