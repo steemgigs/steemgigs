3da1800687e951fb4fe5c6b66c8c2c96b3f31bda
Tobias <tobias.gooney@icloud.com>
2019-02-10 16:30:21 +0000

    Save & get drafts on untalented editor

--- src/pages/platform/editors/untalented.vue
+++ src/pages/platform/editors/untalented.vue
@@ -211,7 +211,20 @@ export default {
       return `<h2 class="headline">Description</h2><hr />${Util.convertImageUrlToHTML(this.untalented.description)}`
     }
   },
+  watch: {
+    'untalented': {
+      handler: function () {
+        this.saveDraft('untalented', this.untalented)
+      },
+      deep: true
+    }
+  },
   mounted () {
+    // Get draft from local storage using mixin
+    const draft = this.getDrafts('untalented')
+    if (draft) {
+      this.untalented = draft
+    }
     this.$eventBus.$on('img-uploaded', payload => {
       console.log(payload)
       this.untalented.portfolio[payload.index] = payload.url
