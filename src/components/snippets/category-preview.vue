66914fcdab2788dd4085a4f4f1b3f89140f2f18b
Tobias <tobias.gooney@icloud.com>
2019-03-02 22:35:49 +0000

    Create category-preview.vue

---
+++ src/components/snippets/category-preview.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'category-preview',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="css">
+    
+</style>
