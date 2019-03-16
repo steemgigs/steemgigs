d1defddf01b4867ddd481051369cc03610e8e643
Tobias <tobias.gooney@icloud.com>
2019-03-16 03:44:02 +0000

    Create user-search.vue

---
+++ src/pages/platform/user-search.vue
@@ -0,0 +1,17 @@
+<template>
+    <page :pageClasses="['user-search__view', 'row']">
+        <el-main>
+        </el-main>
+    </page>
+</template>
+
+<script>
+export default {
+  name: 'user-search',
+  data () {}
+}
+</script>
+
+<style>
+
+</style>
