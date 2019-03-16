2b75ec9706556c2e86e0dca617daddadccd71395
Tobias <tobias.gooney@icloud.com>
2019-03-16 13:37:02 +0000

    Add route and basic template for profile edit page

---
+++ src/pages/platform/edit-profile.vue
@@ -0,0 +1,23 @@
+<template>
+<page :pageClasses="['edit-profile__view', 'row']">
+<el-main>
+</el-main>
+</page>
+</template>
+
+<script>
+
+export default {
+  name: 'edit-profile',
+  data () {
+    return {
+      userProfile: null
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
--- src/pages/router.js
+++ src/pages/router.js
@@ -2,6 +2,7 @@ import Index from '@/pages/index'
 import LoggedIn from '@/pages/access/loggedIn'
 import featured from '@/pages/platform/featured'
 import Profile from '@/pages/platform/profile'
+import EditProfile from '@/pages/platform/edit-profile'
 import GigDetail from '@/pages/platform/gigDetail'
 import Categories from '@/pages/platform/category'
 import SubCategories from '@/pages/platform/subcategory'
@@ -132,6 +133,11 @@ export default [
     path: '/@:username',
     name: 'STEEMGIGS | Profile',
     component: Profile
+  },
+  {
+    path: '/@:username/edit',
+    name: 'STEEMGIGS | Edit Profile',
+    component: EditProfile
   },
   {
     path: '/@:username/:task',
