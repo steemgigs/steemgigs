25415f41b0633021124e4ac9b9feae252a487bfb
Tobias <tobias.gooney@icloud.com>
2019-03-30 13:14:43 +0000

    Add additional fields to profile extras

--- src/components/profile/profile-extras.vue
+++ src/components/profile/profile-extras.vue
@@ -3,14 +3,35 @@
    <div class="profile-extras-card">
       <div class="moreProfileInfo">
          <div class="card-content">
-            <span class="card-title">Links</span>
+           <h5 class="card-title">Skills</h5>
+           <el-tag size="small" v-for="(skill, index) in profile.skillsAndHobbies" :key="index"> {{ skill }} </el-tag>
+           <p v-if="profile.skillsAndHobbies.length === 0">
+             Not Available
+           </p>
+           <h5 class="card-title">Learning</h5>
+           <el-tag size="small" v-for="(learn, index) in profile.learning" :key="index"> {{ learn }} </el-tag>
+           <p v-if="profile.learning.length === 0">
+             Not Available
+           </p>
+           <h5 class="card-title">Help With</h5>
+           <el-tag size="small" v-for="(help, index) in profile.helpWith" :key="index"> {{ help }} </el-tag>
+           <p v-if="profile.helpWith.length === 0">
+             Not Available
+           </p>
+           <h5 class="card-title">I made this</h5>
+           <p>{{ profile.portfolio.description || 'Not Available' }}</p>
+           <h5 class="card-title">Portfolio URL</h5>
+           <p>{{ profile.portfolio.url || 'Not Available' }}</p>
+            <h5 class="card-title">Links</h5>
             <p class="social-link" v-for="(social, key, index) in profile.social" :key="index">
-               <span class="site-label">{{ key }}:</span><span class="site-link" v-html="(social.match(/^(http|https):/)) ? `<a target='_blank' href='${social}'>${social}</a>` : social || 'Link not provided'"/>
+               <span class="site-label">{{ key }}: </span><span class="site-link" v-html="(social.match(/^(http|https):/)) ? `<a target='_blank' href='${social}'>${social}</a>` : social || ' Link not provided'"/>
             </p>
-            <span class="card-title">Languages</span>
+            <h5 class="card-title">Social Reach</h5>
+            <p> {{ profile.socialReach || 'N/A' }} </p>
+            <h5 class="card-title">Languages</h5>
             <ul>
                <li v-for="(language, index) in profile.languages" :key="index">{{`${language} - Fluent`}}</li>
-               <li class="not-avail" v-if='profile.languages.length === 0'>No Languages Provided</li>
+               <p class="not-avail" v-if='profile.languages.length === 0'>No Languages Provided</p>
             </ul>
          </div>
       </div>
@@ -35,23 +56,10 @@ export default {
 .profile-extras-card {
     border-radius: 10px;
     background: white;
-    padding: 20px;
+    padding: 0 20px;
     box-shadow: 0 3px 13px rgba(0, 0, 0, 0.05);
     display: flex;
     flex-direction: column;
-    .card-title {
-      font-size: 1.2em;
-      font-weight: 600;
-      text-align: left;
-      &:first-child {
-        font-size: 1.2em;
-        margin-top: -0.5em;
-        margin-bottom: 0em;
-      }
-      &:not(:first-child) {
-        margin-top: 1em;
-      }
-    }
     ul {
       margin-top: 0;
     }
