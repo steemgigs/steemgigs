3ebc9c3ff7be10477effa871560af03882ee69cd
Tobias <tobias.gooney@icloud.com>
2019-03-31 16:02:27 +0100

    Strip placeholder items from wallet

--- src/pages/platform/wallet.vue
+++ src/pages/platform/wallet.vue
@@ -1,23 +1,19 @@
 <template>
-<page :pageClasses="['placeholder__view', 'row']">
+<page :pageClasses="['wallet__view', 'row']">
 <el-main>
-    <placeholder :header="placeholderHeader" />
+
 </el-main>
 </page>
 </template>
 
 <script>
-
-import placeholder from '@/components/snippets/placeholder'
-
 export default {
   name: 'wallet',
   components: {
-    placeholder
   },
   data () {
     return {
-      placeholderHeader: 'Wallet coming soon'
+   
     }
   }
 }
