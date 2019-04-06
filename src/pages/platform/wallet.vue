41dfdab16b61ab68587ae3df3d27869a15e48131
Tobias <tobias.gooney@icloud.com>
2019-04-06 16:35:35 +0100

    Configure real balances, beautify html

--- src/pages/platform/wallet.vue
+++ src/pages/platform/wallet.vue
@@ -1,27 +1,70 @@
 <template>
-<page :pageClasses="['wallet__view', 'row']">
-<el-main>
-  <!-- Balance List -->
-  <el-row :gutter="20">
-    <!-- Steem Balance -->
-    <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-    </el-col>
-    <!-- SBD Balance -->
-    <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-    </el-col>
-    <!-- Teardrops Balance -->
-    <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-    </el-col>
-  </el-row>
-  <!-- Transaction List-->
-   <el-row :gutter="20">
-     <!-- Transaction Item -->
-     <el-col v-for="(transaction, index) in transactions" :key="index" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-       <transactionrow :transaction="transaction" :user="username"/>
-    </el-col>
-   </el-row>
-</el-main>
-</page>
+   <page :pageClasses="['wallet__view', 'row']">
+      <el-main>
+         <!-- Balance List -->
+         <el-row :gutter="20">
+            <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+               <div class="balance-container">
+                  <!-- Steem Balance -->
+                  <div class="balance-row">
+                     <el-col :xs="12" :sm="12" :md="18" :lg="18" :xl="18">
+                        <div class="details">
+                           <span>Steem</span>
+                           <p>Tradeable tokens that may be transferred anywhere at anytime. Steem can be converted to STEEM POWER in a process called powering up. </p>
+                        </div>
+                     </el-col>
+                     <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6">
+                        <span class="amount">{{ balances.steem_balance }}</span>
+                     </el-col>
+                  </div>
+                  <!-- Steem Power Balance -->
+                  <div class="balance-row">
+                     <el-col :xs="12" :sm="12" :md="18" :lg="18" :xl="18">
+                        <div class="details">
+                           <span>Steem Power</span>
+                           <p>Influence tokens which give you more control over post payouts and allow you to earn on curation rewards.</p>
+                        </div>
+                     </el-col>
+                     <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6">
+                        <span class="amount">{{ steemPower }}</span>
+                     </el-col>
+                  </div>
+                  <!-- SBD Balance -->
+                  <div class="balance-row">
+                     <el-col :xs="12" :sm="12" :md="18" :lg="18" :xl="18">
+                        <div class="details">
+                           <span>SBD</span>
+                           <p>Tradeable tokens that may be transferred anywhere at anytime.</p>
+                        </div>
+                     </el-col>
+                     <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6">
+                        <span class="amount">{{ balances.sbd_balance }}</span>
+                     </el-col>
+                  </div>
+                  <!-- Teardrops Balance -->
+                  <div class="balance-row">
+                     <el-col :xs="12" :sm="12" :md="18" :lg="18" :xl="18">
+                        <div class="details">
+                           <span>Teardrops</span>
+                           <p>A steem-based token that you can use across steemgigs.org, ulogs.org and macrohard.pro. Teardrops can be transfered, used or kept to be powered up in the near future.</p>
+                        </div>
+                     </el-col>
+                     <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6">
+                        <span class="amount">{{ balances.teardrop_balance }} TEARDROPS</span>
+                     </el-col>
+                  </div>
+               </div>
+            </el-col>
+         </el-row>
+         <!-- Transaction List-->
+         <el-row :gutter="20">
+            <!-- Transaction Item -->
+            <el-col v-for="(transaction, index) in transactions" :key="index" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+               <transactionrow :transaction="transaction" :user="username"/>
+            </el-col>
+         </el-row>
+      </el-main>
+   </page>
 </template>
 
 <script>
@@ -43,6 +86,15 @@ export default {
     this.username = this.$route.params.username
     this.getBalances()
     this.getTransactions()
+  },
+  computed: {
+    steemPower () {
+      if (this.balances.delegated_steem_power !== 0) {
+        return this.balances.steem_power + ` (${this.balances.delegated_steem_power.toFixed(3)}) SP`  
+      } else {
+        return this.balances.steem_power + 'SP'
+      }
+    }
   },
   methods: {
     async getBalances () {
@@ -76,6 +128,30 @@ export default {
 
 </script>
 
-<style>
-
+<style lang="scss">
+.balance-container {
+    background: white;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    padding: 15px;
+.balance-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  .details {
+    span {
+      font-weight: 500;
+      color: #181566;
+    }
+    p {
+      margin-top: 2.5px;
+    }
+  }
+  .amount {
+    text-align: right;
+    display: block;
+  }
+}
+}
 </style>
