1596d97bfeb5c71951fe48129466b4ec50372a3e
surpassinggoogle <comsilbronze4@gmail.com>
2019-05-27 22:36:54 +0800

    Revert "Card resizing to get 5 cards per row in desktop resolution."

--- src/components/snippets/category-preview.vue
+++ src/components/snippets/category-preview.vue
@@ -10,7 +10,7 @@
         </div>
         <!-- If there are posts available to be shown, show posts -->
         <div class="post-preview-wrapper" v-if="searchResults.length !== 0">
-        <el-col :xs="24" :sm="12" :md="6" :lg="5" v-for="(post, index) in sortedResults" :key="index">
+        <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6" v-for="(post, index) in sortedResults" :key="index">
             <gig-card v-loading="isLoading" :gigData="post" v-if="post_type !== 'steemgigs_testimonial'" />
             <testimonial-card v-else :testimonial="post" />
         </el-col>
